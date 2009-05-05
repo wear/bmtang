e7a386dae2bcb765dffa6006a6ef7bda66aba90f
wear <wear63659220@gmail.com>
2009-05-05 23:00:00 +0800

    new design

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,5 +2,9 @@ module GroupsHelper
   def group_admin?(admin_role)
     admin_role ? '管理员' : '成员'
   end
+  
+  def show_description(group)
+    group.description ? '暂无介绍' : h(group.description)
+  end
 
 end
